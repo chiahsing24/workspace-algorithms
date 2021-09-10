3082c35eb5c06517c6b4217ae13afba654701e9e
chiahsing24 <chiahsingtony@gmail.com>
2021-09-09 22:08:42 -0700

    Leetcode 0028 added

---
+++ Leetcode/leetcode-0028-ImplementStrStr-[string]-[easy]/ImplementStrStr-[brute-force].java
@@ -0,0 +1,28 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (n > m) {
+            return -1;
+        }
+        
+        for (int i = 0; i < m - n + 1; i++) {
+            int j;
+            for (j = 0; j < n; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
