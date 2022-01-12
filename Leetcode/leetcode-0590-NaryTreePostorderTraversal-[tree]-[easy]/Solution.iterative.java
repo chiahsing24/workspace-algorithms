d5dd1b972c60f3ad995951c6a2f4243408c83eb7
chiahsing24 <chiahsingtony@gmail.com>
2022-01-11 23:09:21 -0800

    Add leetcode0590 iterative solution

--- Leetcode/leetcode-0590-NaryTreePostorderTraversal-[tree]-[easy]/Solution.iterative.java
+++ Leetcode/leetcode-0590-NaryTreePostorderTraversal-[tree]-[easy]/Solution.iterative.java
@@ -1,3 +1,22 @@
-public class Solution.iterative {
-    
+class Solution {
+    public List<Integer> postorder(Node root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+        
+        Node cur = root;
+        Stack<Node> stack = new Stack<>();
+        stack.push(cur);
+        while (!stack.isEmpty()) {
+            cur = stack.pop(); 
+            for (int i = 0; i < cur.children.size(); i++) {
+                if (cur.children.get(i) != null) {
+                    stack.push(cur.children.get(i));
+                }
+            }
+            
+            result.addFirst(cur.val); 
+        }
+        
+        return result;
+    }
 }
