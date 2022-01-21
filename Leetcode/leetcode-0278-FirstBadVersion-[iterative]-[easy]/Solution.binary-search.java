1a771f94b1cf3c3374b75d37def47ec87ea61ea0
chiahsing24 <chiahsingtony@gmail.com>
2022-01-20 23:36:52 -0800

    leetcode0278 added

---
+++ Leetcode/leetcode-0278-FirstBadVersion-[iterative]-[easy]/Solution.binary-search.java
@@ -0,0 +1,24 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid) == true) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (isBadVersion(start) == true) {
+            return start;
+        }
+        
+        if (isBadVersion(end) == true) {
+            return end;
+        }
+        
+        return -1;
+    }
+}
