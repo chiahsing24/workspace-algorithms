32271e24d0507700498a1b3792fa6e350afc2eb4
chiahsingchu <chiahsingtony@gmail.com>
2023-08-19 23:20:39 -0700

    Add more solution for leetcode0283

---
+++ Leetcode/leetcode-0283-MoveZeroes-[array]-[easy]/Solution.two-pointers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  for (let i = 0, j = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      let temp = nums[j];
+      nums[j] = nums[i];
+      nums[i] = temp;
+      j++;
+    }
+  }
+};
